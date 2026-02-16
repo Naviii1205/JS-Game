2462e2914aa62e4d97197322e327e2076632b8b9
Oscar Tiusaba <oatiusaba1205@gmail.com>
2026-02-16 13:59:59 -0500

    cradas arrays y funciones

--- js/main.js
+++ js/main.js
@@ -1,75 +1,63 @@
-let nombreplayer = prompt("Cual es tu nombre?")
+let iniciarg = document.getElementById("iniciar")
+let mostrarpregunta = document.getElementById("mostrarpregunta")
+let resp1 = document.getElementById("resp1")
+let numpregunta = 0
 
-const tipodedecision = ["Buena decision", "Decision regular", "Mala decision"]
+const preguntas = [
+    {
+        id: 1,
+        preguntatxt: "Cuanto es 1 + 1",
+        respuesta1: "2",
+        respuesta2: "3",
+        respuesta3: "4",
+    },
+    {
+        id: 2,
+        preguntatxt: "Cuanto es 2 + 2",
+        respuesta1: "2",
+        respuesta2: "3",
+        respuesta3: "4",
+    },
+    {
+        id: 3,
+        preguntatxt: "Cuanto es 3 + 2",
+        respuesta1: "2",
+        respuesta2: "3",
+        respuesta3: "5",
+    }
+]
 
-const iniciojuego = function () {
-    alert("Hola " + nombreplayer + " bienvenido al juego.\nBasado en tus decisiones tomaras rutas diferentes por el juego.\nEspero lo disfrutes!!!")
-}
 
-const primerapregunta = function () {
-    alert("Aqui va la pregunta " + nombreplayer + " imagina que tienes 6 invitados en casa y decides cocinar que preparas?")
-    let primerarespuesta = parseInt(prompt("Elije una opcion: \n 1-Pollo al Curry \n 2-Pides Pizza a Domicilio \n 3-Carne con Vegetales Salteados\n 4-No Haces Nada se Acaba el Juego"))
-    while (primerarespuesta !== 4) {
-        switch (primerarespuesta) {
-            case 1:
-                pollo()
-                break
-            case 2:
-                pizza()
-                break
-            case 3:
-                carne()
-                break
-            default:
-                alert("Respuesta no valida")
-        }
-        primerarespuesta = parseInt(prompt("Elije una opcion: \n 1-Pollo al Curry \n 2-Pides Pizza a Domicilio \n 3-Carne con Vegetales al Horno\n 4-No cocinas se acaba el juego"))
+function iniciargame(arraydepreguntas) {
+    if (numpregunta == 1){
+        arraydepreguntas.forEach(pregunta => {
+            if (pregunta.id == 1){
+            card = document.createElement("div")
+            card.innerHTML = `<h3>Pregunta No.${pregunta.id}</h3>
+                            <h3>${pregunta.preguntatxt}</h3>
+                            <button id=resp1>${pregunta.respuesta1}</button>
+                            <button id=resp2>${pregunta.respuesta2}</button>
+                            <button id=resp3>${pregunta.respuesta3}</button>`
+            mostrarpregunta.appendChild(card)
+            }
+        })
     }
-}
-
-const pollo = function(numeroA){
-    alert("Empiezas a prepar el pollo.")
-    numeroA = parseInt(prompt("¿Cuantos gramos de curry usas?"))
-    if(numeroA <= 8){
-        alert(tipodedecision[1] + "\nEl sabor te queda plano")
-    }
-    else if(numeroA <= 15){
-        alert(tipodedecision[0] + "\nQueda en el punto correcto de sazon")
-    }
-    else{
-        alert(tipodedecision[2] + "\nDemasiado curry!!!")
-    }
-}
-
-const carne = function(numeroA){
-    alert("Cortas la carne y los vegetelas, preparas el horno a la temperatura ideal")
-    numeroA = parseInt(prompt("¿Cuantos minutos lo dejas en el horno?"))
-    if(numeroA <= 12){
-        alert(tipodedecision[1] + "\nNo te queda bien cocinado pero no sabe mal")
-    }
-    else if(numeroA <= 30){
-        alert(tipodedecision[0] + "\nQueda en el punto ideal de coccion")
-    }
-    else{
-        alert(tipodedecision[2] + "\nSe quemo la comida!!!")
-    }
-}
-
-const pizza = function(numeroA){
-    alert("Llamas a pedir la pizza.")
-    numeroA = parseInt(prompt("¿Cuantas porciones pides?"))
-    if(numeroA <= 5){
-        alert(tipodedecision[2] + "\nMuy pocas porciones!!!")
-    }
-    else if(numeroA <= 12){
-        alert(tipodedecision[0] + "\nPediste lo justo y todos comieron bien")
-    }
-    else{
-        alert(tipodedecision[1] + "\nSobraron porciones, almenos ya sabes que desayunar mañana")
+    else if (numpregunta == 2){
+        arraydepreguntas.forEach(pregunta => {
+            if (pregunta.id == 2){
+            card = document.createElement("div")
+            card.innerHTML = `<h3>Pregunta No.${pregunta.id}</h3>
+                            <h3>${pregunta.preguntatxt}</h3>
+                            <button id=resp1>${pregunta.respuesta1}</button>
+                            <button id=resp2>${pregunta.respuesta2}</button>
+                            <button id=resp3>${pregunta.respuesta3}</button>`
+            mostrarpregunta.appendChild(card)
+            }
+        })
     }
 }
 
-iniciojuego()
-primerapregunta()
-
-
+iniciarg.onclick = () => {
+    numpregunta ++
+    iniciargame(preguntas)
+}
